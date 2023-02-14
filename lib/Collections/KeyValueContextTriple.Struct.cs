46fd274d5a2bd78e27195971572442d3abcb0222
meg <110250578+meg-io@users.noreply.github.com>
2023-02-14 05:37:25 +0000

    Define KeyValueContextTriple Struct
    Add "KeyValueContextTriple"-struct to class-library. By proxy, a class
    library - along with a namespace - was created.
    
    KVC-struct will be used to group related variables needed to transfer
    data from a sender to reciever.
    
    A KVC can also be seen as a nested-KV,(Key1, (Key2, Value)) where 'Key1'
    is needed because Key2 lacks an unique attribute to be used as a proper-
    key. 'Key2' will typically be value-data, so this is often the case.

---
+++ lib/Collections/KeyValueContextTriple.Struct.cs
@@ -0,0 +1,72 @@
+using System;
+
+
+namespace Meg.Delta.Collections
+{
+	[Serializable]
+	public readonly struct KeyValueContextTriple<TKey, TValue>
+	{
+		private readonly TKey _key;
+		private readonly TValue _value;
+		private readonly Action<TValue> _context;
+		
+		
+		public KeyValueContextTriple
+		(
+			TKey key,
+			TValue value,
+			Action<TValue> context
+		)
+		{
+			_key = key;
+			_value = value;
+			_context = context;
+		}
+		
+		
+		public TKey Key
+		{
+			get
+			{
+				return _key;
+			}
+		}
+		
+		public TValue Value
+		{
+			get
+			{
+				return _value;
+			}
+		}
+		
+		public Action<TValue> Context
+		{
+			get
+			{
+				return _context;
+			}
+		}
+		
+		public void Deconstruct
+		(
+			out TKey key,
+			out TValue value,
+			out Action<TValue> context
+		)
+		{
+			key = Key;
+			value = Value;
+			context = Context;
+			
+			return;
+		}
+		
+		public override string ToString
+		(
+		)
+		{
+			return string.Format("[{0}, {1}, {2}]", Key, Value, Context);
+		}	
+	}
+}
